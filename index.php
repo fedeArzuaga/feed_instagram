cee71faa97ee9607e49af73742d8437f1aff85f9
Federico Arzuaga <fede.arzuaga.perdomo@gmail.com>
2021-02-06 21:02:50 -0300

    Agregando peticion cURL al archivo index

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "Hola en Heroku ole!";
+$basic_url = "https://graph.instagram.com/";
+$query_url = "me/media?fields=id,username,timestamp,caption,media_url,media_type,permalink&access_token=";
+$access_token = "IGQVJVQ0FDMmZAPbF9HYVNyOU01dDVJNGhIekZAXM0JlOTJfMVlLajdsbklHUmp2czJWdWh6VU44QzJ1ZADJjQjBjOS1LajJERm1LVHVpYlFXcnd1dGJrWXhiN0F3T3hMZAkE4ZAXNLbFZA6djRrQkN0aHBVSAZDZD";
+
+$client = curl_init();
+curl_setopt($client, CURLOPT_URL, $basic_url.$query_url.$access_token);
+curl_setopt($client, CURLOPT_HEADER, 0);
+curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+$content = curl_exec($client);
+curl_close($client);
+
+echo $content;
 
 ?>
