b8ff0fab034527b7c59f64ddde52d06a5907f192
Federico Arzuaga <fede.arzuaga.perdomo@gmail.com>
2021-02-06 21:12:22 -0300

    Agregando peticion cURL al archivo index

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ curl_setopt($client, CURLOPT_URL, $basic_url.$query_url.$access_token);
 curl_setopt($client, CURLOPT_HEADER, 0);
 curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
 
-$content = json_decode(curl_exec($client));
+$content = json_decode(json_decode(curl_exec($client)));
 curl_close($client);
 
-echo "<pre>"; print_r(json_decode($content)); echo "</pre>";
+echo "<pre>"; print_r($content); echo "</pre>";
 
 ?>
